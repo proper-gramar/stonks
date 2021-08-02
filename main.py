5867d1d6e6619dfc1b5196aeed9e1b705cfbdaf1
proper-gramar <66331084+proper-gramar@users.noreply.github.com>
2021-08-02 15:29:45 -0700

    final final commit
    more formatting, grammar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import yfinance as yf
 
-
 stockInput = input("enter your tickers symbol: ").upper()
 print("valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max")
 periodInput = input("enter desired period:").lower()
@@ -11,14 +10,10 @@ def getLastPrice(ticker):
     stock = yf.Ticker(ticker)
     data = stock.history()
     last_price = (data.tail(1)['Close'].iloc[0])
-    print(last_price)
-
-
-
+    print("{:.2f}".format(last_price))
 
 
 def stockDateRange(ticker, period):
-
     stock = yf.Ticker(ticker)
     data = stock.history(period=period)
     highDate = ""
@@ -38,11 +33,13 @@ def stockDateRange(ticker, period):
     finallowdate = str.replace(lowDate, "00:00:00", "")
     gain = float(high / low)
     pot_shares = 1000 / low
-    print("high:", finalhighdate, "@ $", high)
-    print("low:", finallowdate, "@ $", low)
-    print("if you had bought on", finallowdate, "you would have had a", gain * 100, "% gain")
-    print("if you had invested $1,000 for", pot_shares, "shares, it would be worth $", gain * 1000)
+    print("high:", finalhighdate, "@ $", "{:.2f}".format(high))
+    print("low:", finallowdate, "@ $", "{:.2f}".format(low))
+    print("if you had bought on", finallowdate, "at $", "{:.2f}".format(low) ,"you would have had a",
+          "{:.2f}".format(gain * 100), "% gain")
+    print("if you had invested $1,000 for", "{:.2f}".format(pot_shares), "shares at $", "{:.2f}".format(low),
+          ", it would be worth $", "{:.2f}".format(gain * 1000))
+
 
 print("current price of", stockInput, "is: $", end="", flush=True), getLastPrice(stockInput)
 stockDateRange(stockInput, periodInput)
-
